791b063c105b740202d53c4d2c722d289a44076a
Abhishek2291 <abhishekgajera2017@gmail.com>
2023-03-25 11:43:24 +0530

    folder structure changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import About from './components/About'
+import Home from './components/Home'
+
+const App = () => {
+  return <div>
+    <Home />
+    <About />
+  </div>
+}
+
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import Banner from './HomeElements/Banner'
+
+const Home = () => {
+    return <div>
+        Home Banner
+        <Banner />
+    </div>
+}
+
+export default Home
